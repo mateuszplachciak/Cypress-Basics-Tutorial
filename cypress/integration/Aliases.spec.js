700c4ad8e3bf50c16a1216009e087d2f5de6bb50
Kormak <m.l.plachciak@gmail.com>
2020-01-22 15:17:31 +0100

    Aliases

---
+++ cypress/integration/Aliases.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Test of shop Flow',() => {
+    beforeEach('Navigating to Page',() => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    })
+    it('Lets make some Aliases',() => {
+        cy.get('.search-keyword').as('search').type('Capsicum').wait(1000)
+        cy.get('.product').find('.product-name').as('productName').should('include.html', 'Capsicum')
+        cy.get('.product').find('button').contains('ADD TO CART').as('addProduct').click()
+    
+    
+        cy.get('@search').type('{selectAll}Carrot').wait(1000)
+        cy.get('@productName').should('include.html', 'Carrot')
+        cy.get('@addProduct').click()
+    })
+    
+})
